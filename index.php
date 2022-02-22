060683a4aed2111c241c97231fe3a9375bea199c
Adriano Xavier <aj-dx@hotmail.com>
2022-02-22 12:34:47 -0300

    Iniciando projeto

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'src/header.php';
+?>
+    <div class="card">
+        <div class="card-header text-center bg-default">
+            G-mode
+        </div>
+        <div class="item_card">
+            <p style="text-align: center;">Sistema criado para geração de código fonte automatizado.</p>
+        </div>
+    </div>
+</div>
+<script>
+<?php
+require_once 'src/footer.php';
