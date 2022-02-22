e08ed74d0c187a21113d4881ddfe21aa01add3ea
Adriano Xavier <aj-dx@hotmail.com>
2022-02-22 17:54:30 -0300

    Testando conexões

--- index.php
+++ index.php
@@ -1,5 +1,33 @@
 <?php
 require_once 'src/header.php';
+
+
+//Criar as constantes com as credencias de acesso ao banco de dados
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASS', '');
+define('DBNAME', '');
+
+/* $dsn = 'mysql:dbname=testdb;host=127.0.0.1';
+$user = 'dbuser';
+$password = 'dbpass';
+
+$dbh = new PDO($dsn, $user, $password); */
+
+//Criar a conexão com banco de dados usando o PDO e a porta do banco de dados
+//Utilizar o Try/Catch para verificar a conexão.
+try {
+    $conn = new pdo('mysql:host=' . HOST . ';dbname=' . DBNAME, USER, PASS);
+    echo "Conexão com banco de dados realizada com sucesso.";
+} catch (PDOException $e) {
+    echo "Erro: Conexão com banco de dados não foi realizada com sucesso. Erro gerado " . $e->getMessage();
+}
+
+$query = $conn->query("show databases");
+$resultado = $query->fetchAll(PDO::FETCH_ASSOC);
+foreach($resultado as $item){
+   echo $item['Database']."<br>";
+}
 ?>
     <div class="card">
         <div class="card-header text-center bg-default">
