0b84c90d81d46a9b67235af02ea8a1125d06aca3
Adriano Xavier <aj-dx@hotmail.com>
2022-02-24 16:40:28 -0300

    Add new functions

--- src/functions.php
+++ src/functions.php
@@ -6,8 +6,40 @@ function listDatabase($conn){
 }
 
 function listTable($conn, $base){
-    $sql = "SELECT *FROM INFORMATION_SCHEMA.COLUMNS AS c WHERE
-                c.TABLE_SCHEMA = '$base'";
+    $sql = "SELECT 
+                TABLE_NAME
+            FROM
+                information_schema.tables
+            WHERE
+                table_schema = '$base'";
+               
+    $query = $conn->query($sql);
+    $resultado = $query->fetchAll(PDO::FETCH_ASSOC);
+    return $resultado;
+}
+
+function listColumns($conn, $base, $table){
+    $sql = "SELECT 
+                *
+            FROM
+                INFORMATION_SCHEMA.COLUMNS AS c
+            WHERE
+                c.TABLE_SCHEMA = '$base'
+                    AND c.TABLE_NAME = '$table'";
+               
+    $query = $conn->query($sql);
+    $resultado = $query->fetchAll(PDO::FETCH_ASSOC);
+    return $resultado;
+}
+
+function listColunsUsage($conn, $base, $table){
+    $sql = "SELECT 
+                *
+            FROM
+                INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+            WHERE
+                table_schema = '$base'
+                    AND table_name = '$table'";
                
     $query = $conn->query($sql);
     $resultado = $query->fetchAll(PDO::FETCH_ASSOC);
